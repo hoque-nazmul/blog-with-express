12616fd8c89ed51b41747fcb4d08b00ccc5ee7e1
Nazmul Hoque <coder.nazmul@gmail.com>
2020-07-22 16:32:03 +0600

    user model created

---
+++ models/User.js
@@ -0,0 +1,31 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 30
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    profile: {
+        type: Schema.Types.ObjectId,
+        ref: "Profile"
+    }
+}, {
+    timestamps: true
+});
+
+const User = model("User", userSchema);
+
+module.exports = User;
