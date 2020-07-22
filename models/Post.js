6ce8880a993a78fb50ce57c73b5d4e67cf04b933
Nazmul Hoque <coder.nazmul@gmail.com>
2020-07-23 00:29:26 +0600

    Post model design done

---
+++ models/Post.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require('mongoose');
+
+const postSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 180
+    }, 
+    body: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    tags: {
+        type: [String],
+        required: true,
+    },
+    thumbnail: String,
+    readTime: String,
+    likes: [Schema.Types.ObjectId],
+    dislikes: [Schema.Types.ObjectId],
+    comments: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Comment'
+        }
+    ]
+})
+
+const Post = model('Post', postSchema);
+
+module.exports = Post;
