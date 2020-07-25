c3c648d13a8cb2efe4cc68bebd1b10153c8be058
Nazmul Hoque <coder.nazmul@gmail.com>
2020-07-25 11:08:31 +0600

    sign up & login page's data handling done

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -26,10 +26,34 @@ exports.signupPostController = async (req, res, next) => {
 }
 
 exports.loginGetController = (req, res, next) => {
-
+    res.render('pages/login', {title: "Login to your Account"})
 }
 
-exports.loginPostController = (req, res, next) => {
+exports.loginPostController = async (req, res, next) => {
+    const {email, password} = req.body;
+
+    try {
+        const user = await User.findOne({email})
+        if(!user) {
+            return res.json({
+                message: "Invalid email or password"
+            })
+        }
+
+        const match = await bcrypt.compare(password, user.password)
+        if(!match) {
+            res.json({
+                message: "Invalid email or password"
+            })
+        } else {
+            console.log("user login successfully", user);
+            res.render('pages/login', {title: "Login to your Account"});
+        }
+
+    } catch (err) {
+        console.log(err);
+        next(err);
+    }
 
 }
 
