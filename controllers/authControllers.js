5753165fae1a00e582fd13b7bb7072f80bab03e2
Nazmul Hoque <coder.nazmul@gmail.com>
2020-07-25 10:46:56 +0600

    bcrypt added

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,22 +1,24 @@
+const bcrypt = require('bcrypt');
 const User = require("../models/User");
 
 exports.signupGetController = (req, res, next) => {
-    res.render('pages/signup', {title: "Create a new Account"});
+    res.render('pages/signup', { title: "Create a new Account" });
 }
 
 exports.signupPostController = async (req, res, next) => {
     const { username, email, password } = req.body;
-    
-    const user = new User({
-        username,
-        email,
-        password
-    });
-    
+
     try {
+        const hashedPassword = await bcrypt.hash(password, 11)
+        const user = new User({
+            username,
+            email,
+            password: hashedPassword
+        });
+
         const createdUser = await user.save()
         console.log("user created successfully");
-        res.render('pages/signup', {title: "Create a new Account"});
+        res.render('pages/signup', { title: "Create a new Account" });
     } catch (err) {
         console.log(err);
         next(err)
@@ -25,7 +27,7 @@ exports.signupPostController = async (req, res, next) => {
 
 exports.loginGetController = (req, res, next) => {
 
-} 
+}
 
 exports.loginPostController = (req, res, next) => {
 
