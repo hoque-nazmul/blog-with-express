97268322706e1a47f5e5460e54e144b9df97d5a3
Nazmul Hoque <coder.nazmul@gmail.com>
2020-07-27 00:15:51 +0600

    added sample of .env file

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
-const session = require('express-session');
+const session = require('express-session'); 
 const flash = require('connect-flash');
 const MongoDBStore = require('connect-mongodb-session')(session);
 require('dotenv').config();
